3f14b08416e5614934e81d8a34108b2960bd665a
nagesh <nagesh7460@gmail.com>
2022-11-14 19:49:08 +0530

    fourth

---
+++ db.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        email: "nagesh7460@gmail.com",
+        password: "123456"
+    }
+]
+
+const publicPosts = [
+    {
+        title: "Free Tips on Development",
+        content: "These are some tips"
+    }
+]
+
+const privatePosts = [
+    {
+        title: "Paid Tips on Development",
+        content: "These are some tips"
+    }
+]
+
+module.exports = {
+    users,
+    publicPosts,
+    privatePosts
+}
